5b09cce8b96c12d05be6cd5495a9953b25d45008
Jerry Kiely <jerry@cowboysmall.com>
2021-05-19 22:53:12 +0100

    some further cleanup

--- game/tictactoe.py
+++ game/tictactoe.py
@@ -8,15 +8,15 @@ def row_string(row):
     return '  {} | {} | {}  \n'.format(row[0], row[1], row[2])
 
 def board_string(board):
-    return ' ---+---+--- \n'.join([row_string(board[(i * 3):(i * 3) + 3]) for i in range(3)]) + '\n'
+    return '{}\n'.format(' ---+---+--- \n'.join([row_string(board[(i * 3):(i * 3) + 3]) for i in range(3)]))
 
 
 
 def legal_moves(board):
     return [i for i in range(len(board)) if board[i] == '.']
 
-def make_move(board, position, player):
-    return [player if i == position else board[i] for i in range(len(board))]
+def make_move(board, move, player):
+    return [player if i == move else board[i] for i in range(len(board))]
 
 def next(player):
     return 'X' if player == 'O' else 'O'
@@ -45,11 +45,11 @@ def game_over(board):
 
 
 
-def get_move(values):
+def get_move(moves):
     try:
-        return int(requests.get('http://localhost:5000/random/choice?{}'.format(join_values(values))).json()['value'])
+        return int(requests.get('http://localhost:5000/random/choice?{}'.format(join_values(moves))).json()['value'])
     except:
-        return random.choice(values)
+        return random.choice(moves)
 
 def join_values(values):
     return '&'.join(['value={}'.format(value) for value in values])
